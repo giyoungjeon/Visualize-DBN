cb97cea3bac548f7631f7a36e319a36773a1075d
Freestar51 <freestar51@unist.ac.kr>
2015-04-09 17:21:34 +0900

    parfor & debug

--- grad_ascent.m
+++ grad_ascent.m
@@ -1,11 +1,8 @@
 function [ X_ ] = grad_ascent( D, rho, n )
-%GRAD_ASCENT Summary of this function goes here
-%   Detailed explanation goes here
     function [vh] = tarfun(X)
         vh = X;
         for l = 2:3
             vh = bsxfun(@plus, vh * D.rec.W{l-1}, D.rec.biases{l}');
-%             vh = binornd(1, vh);
         end        
         vh = bsxfun(@plus, vh * D.top.W, D.top.hbias');
         vh = double(vh(n));
@@ -14,29 +11,26 @@ function [ X_ ] = grad_ascent( D, rho, n )
     maxiter=100;
     learn_rate = 0.01;
     delta = 14/1000;
-    %delta = 0.0001;
+    sample_n = 1;
     X_ = zeros(1,14*14);
-    for i=1:100
+    for samp_i=1:sample_n
         X0 = rand(14);
         X0 = X0 / norm(X0)*rho;
         X0 = reshape(X0, 14*14,1)';
         for iter=1:maxiter
             grad = zeros(size(X0));
-            if  mod(iter,10)==0
-%                 disp(sprintf('%dth iter', iter));
-            end
             for i=1:size(X0,2)
-                X = X0; X = X0;
-                X(i) = X(i)-delta;
-                y1 = tarfun(X);%/norm(X1)*rho);
+                X = X0;
+                y1 = tarfun(X);
                 X(i) = X(i)+delta;
-                y2 = tarfun(X);%/norm(X2)*rho);
-                grad(i) = (y2-y1)/(2*delta);
+                y2 = tarfun(X);
+                grad(i) = (y2-y1)/(delta);
                 X0= X0 + grad*learn_rate;
                 X0=X0/norm(X0)*rho;
             end
+            ex_grad = grad;
         end
         X_ = X_ + X0;
     end
-    X_=X_/100;
+    X_=X_/sample_n;
 end
--- visualize_dbn.m
+++ visualize_dbn.m
@@ -3,10 +3,15 @@ load('dbn_mnist_D.mat');
 rho=10;
 warning('off','all');
 figure;
-for i=1:100
-    disp(sprintf('optimizing unit %d\n',i));
-    x_ = grad_ascent(D,rho,i);
-    subplot(10,10,i);
-    imshow(reshape(x_, 14, 14)');
-%     title(sprintf('unit %d\n',i));
+mult = 0:10:990;
+u_idx = floor(mod(rand(1,100)*100,10));
+u_idx = u_idx + mult;
+x_ = cell(1,100);
+parfor idx = 1:100
+    disp(sprintf('optimizing unit %d\n',u_idx(idx)));
+    x_{idx} = grad_ascent(D,rho,u_idx(idx));
+end
+for idx = 1:100
+    subplot(10,10,idx);
+    imshow(reshape(x_{idx}, 14, 14)');
 end
